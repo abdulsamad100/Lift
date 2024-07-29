bb6e951f32467d6eac7ee41b817080046ca82b72
Abdul Samad <samadjs100@gmail.com>
2024-07-29 21:13:32 +0500

    update 1

--- app.js
+++ app.js
@@ -1,35 +1,4 @@
-const lift=document.querySelector("#lift");
-const changeFloor = (floorNum)=>{
-    if(floorNum==0){
-        lift.src="Lift-Close.png"
-        lift.className="";
-        lift.classList.add("ground");
-        setTimeout(()=>{
-            lift.src="Lift-Open.png"
-        },1000)
-    }
-    else if(floorNum==1){
-        lift.src="Lift-Close.png"
-        lift.className="";
-        lift.classList.add("first");
-        setTimeout(()=>{
-            lift.src="Lift-Open.png"
-        },1000)
-    }
-    else if(floorNum==2){
-        lift.src="Lift-Close.png"
-        lift.className="";
-        lift.classList.add("second");
-        setTimeout(()=>{
-            lift.src="Lift-Open.png"
-        },1000)
-    }
-    else if(floorNum==3){
-        lift.src="Lift-Close.png"
-        lift.className="";
-        lift.classList.add("third");
-        setTimeout(()=>{
-            lift.src="Lift-Open.png"
-        },1000)
-    }
+const lift = document.querySelector("#lift");
+const changeFloor = (floorNum) => {
+    lift.style.paddingBottom = `${floorNum}px`;
 }
